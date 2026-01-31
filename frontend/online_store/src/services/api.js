9b2c081361da3686d15b749328e717bc3e3a85cd
Gabriel Lemuchi <lemuchigabriel@gmail.com>
2026-01-31 20:42:10 -0300

    new axios

--- frontend/online_store/src/services/api.js
+++ frontend/online_store/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: [ 'http://localhost:3000', 'https://online-store-7c9e.onrender.com']
 });
 
 api.interceptors.request.use(config => {
