461b753b8beda22115851f2b1572f44f523a485b
Gabriel Lemuchi <lemuchigabriel@gmail.com>
2026-01-31 21:03:28 -0300

    new cors

--- backend/api_store/src/app.js
+++ backend/api_store/src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://onlinestore-bice.vercel.app"
+];
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://onlinestore-bice.vercel.app/"],
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
