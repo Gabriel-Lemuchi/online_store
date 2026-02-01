3d3b90874556d396e0d242644269d37d5d22b982
Gabriel Lemuchi <lemuchigabriel@gmail.com>
2026-01-31 21:00:15 -0300

    new cors

--- backend/api_store/src/app.js
+++ backend/api_store/src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://online-store-7c9e.onrender.com/"],
+  origin: ["http://localhost:5173", "https://onlinestore-bice.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
